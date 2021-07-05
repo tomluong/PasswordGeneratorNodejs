e267b39c0a2aab26b422c51734727bb569dd29e6
Tom Luong <tomluong@live.com>
2021-07-05 17:50:57 +0000

    Add program options

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const chalk = require('chalk')
+const program = require('commander')
 
-chalk.yellow('hello world')
+program.version('1.0.0').description('Simple Password Generator')
 
-chalk.blue('hi again')
+program
+.option('-l, --length <number>', 'length of password', 8)
+.option('-s, --save', 'save password to password.txt')
+.option('-nn, --no-numbers', 'remove numbers')
+.option('-ns, --no-symbols', 'remove symbols')
+.parse()
+
+console.log(program.opts())
