0395884f8be3b606ab28635ccf8e6b7568c07271
Tom Luong <tomluong@live.com>
2021-07-05 18:12:22 +0000

    remove unused option

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ program.version('1.0.0').description('Simple Password Generator')
 
 program
 .option('-l, --length <number>', 'length of password', 8)
-.option('-s, --save', 'save password to password.txt')
 .option('-nn, --no-numbers', 'remove numbers')
 .option('-ns, --no-symbols', 'remove symbols')
 .parse()
